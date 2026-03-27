841c7b47100c29cffc62f1cbffbb8b1ffc35c0a4
mbmanoj904-hue <mbmanoj904@gmail.com>
2026-03-27 10:05:47 +0530

    Initial commit for java Project

---
+++ Pattern2.java
@@ -0,0 +1,11 @@
+public class Pattern2 {
+    public static void main(String[] args) {
+        int n=6;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<5;j++){
+                System.out.print("* ");
+        }
+      System.out.println();
+    }
+ }
+}
